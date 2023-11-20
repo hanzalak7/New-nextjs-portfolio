e23f5c64c09df6b91864bd864209a582e0dbfc96
unknown <hkjava6@gmail.com>
2023-11-20 23:26:08 +0530

    fixing errors

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,53 +8,53 @@ import Image3 from '../../public/Images/project3.png'
 import Image4 from '../../public/Images/project4.png'
 import Image5 from '../../public/Images/project5.png'
 
-// const projectData = [
-//     {
-//         id:3,
-//         title:'Netflix Clone',
-//         description:'A functional clone of netflix with passwordless authentication and with using google apis for data.',
-//         imgUrl:Image4.src,
-//         gitUrl:'https://github.com/Ayush2004namdev/netflix-clone.git',
-//         previewUrl:'https://netflix-clone-six-fawn.vercel.app/',
-//         tags:['All','Web']
-//     },
-//     {
-//         id:1,
-//         title:'Coffee Connosoure ',
-//         description:'A easy to use web application in finding Coffee stores near you with the help of geolocations.',
-//         imgUrl:Image1.src,
-//         gitUrl:'https://github.com/Ayush2004namdev/find-coffee-store.git',
-//         previewUrl:'https://find-stores-coffee.vercel.app/',
-//         tags:['All','Web']
-//     },
-//     {
-//         id:2,
-//         title:'Yelp Camp',
-//         description:'A fully functional crud app for creating and validating users and posts with reviews and many more options.',
-//         imgUrl:Image2.src,
-//         gitUrl:'https://github.com/Ayush2004namdev/yelp-camp.git',
-//         previewUrl:'https://enthusiastic-slug-miniskirt.cyclic.app/',
-//         tags:['All','Web']
-//     },
-//     {
-//         id:4,
-//         title:'Raw Fitness',
-//         description:'A website for a Gym to showcase its features and courses to enroll for a better public approach and more attention.',
-//         imgUrl:Image5.src,
-//         gitUrl:'https://github.com/Ayush2004namdev/fitness.git',
-//         previewUrl:'https://raw-fitness-iimq095r1-ayush2004namdev.vercel.app/',
-//         tags:['All','Web']
-//     },
-//     {
-//         id:5,
-//         title:'Organic store',
-//         description:'A basic website template for a ecommerce website written in pure html and css.',
-//         imgUrl:Image3.src,
-//         gitUrl:'https://github.com/Ayush2004namdev/ecom.git',
-//         previewUrl:'https://ayush2004namdev.github.io/ecom/',
-//         tags:['All','Web']
-//     },
-// ]
+const projectData = [
+    {
+        id:3,
+        title:'Netflix Clone',
+        description:'A functional clone of netflix with passwordless authentication and with using google apis for data.',
+        imgUrl:Image4.src,
+        gitUrl:'https://github.com/Ayush2004namdev/netflix-clone.git',
+        previewUrl:'https://netflix-clone-six-fawn.vercel.app/',
+        tags:['All','Web']
+    },
+    {
+        id:1,
+        title:'Coffee Connosoure ',
+        description:'A easy to use web application in finding Coffee stores near you with the help of geolocations.',
+        imgUrl:Image1.src,
+        gitUrl:'https://github.com/Ayush2004namdev/find-coffee-store.git',
+        previewUrl:'https://find-stores-coffee.vercel.app/',
+        tags:['All','Web']
+    },
+    {
+        id:2,
+        title:'Yelp Camp',
+        description:'A fully functional crud app for creating and validating users and posts with reviews and many more options.',
+        imgUrl:Image2.src,
+        gitUrl:'https://github.com/Ayush2004namdev/yelp-camp.git',
+        previewUrl:'https://enthusiastic-slug-miniskirt.cyclic.app/',
+        tags:['All','Web']
+    },
+    {
+        id:4,
+        title:'Raw Fitness',
+        description:'A website for a Gym to showcase its features and courses to enroll for a better public approach and more attention.',
+        imgUrl:Image5.src,
+        gitUrl:'https://github.com/Ayush2004namdev/fitness.git',
+        previewUrl:'https://raw-fitness-iimq095r1-ayush2004namdev.vercel.app/',
+        tags:['All','Web']
+    },
+    {
+        id:5,
+        title:'Organic store',
+        description:'A basic website template for a ecommerce website written in pure html and css.',
+        imgUrl:Image3.src,
+        gitUrl:'https://github.com/Ayush2004namdev/ecom.git',
+        previewUrl:'https://ayush2004namdev.github.io/ecom/',
+        tags:['All','Web']
+    },
+]
 
 
 
